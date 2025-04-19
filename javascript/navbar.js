c75bd1c8657db77edb8f7557dd3cb4456c925220
mustakimbillah369 <billah.mustakim30401@gmail.com>
2025-04-19 21:13:21 +0600

    added blog page and card section

--- javascript/navbar.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('bg-btn').addEventListener('click', function () {
-  document.documentElement.style.backgroundColor = bgRandomColor();
-})
---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+document.getElementById('bg-btn').addEventListener('click', function () {
+  document.documentElement.style.backgroundColor = bgRandomColor();
+})
+
+document.getElementById('blog').addEventListener('click', function() {
+  window.location.href = "blog.html";
+})
+
+
+// 
+
+const dateBox = document.getElementById('date-box');
+
+const now = new Date();
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+const formattedDate = now.toLocaleString('en-us', options);
+dateBox.textContent = "" + formattedDate;
