5f11135c018b29ae7c179824c8976bf43fae9acf
mustakimbillah369 <billah.mustakim30401@gmail.com>
2025-04-19 23:38:35 +0600

    add javascript

--- javascript/script.js
+++ javascript/script.js
@@ -16,11 +16,32 @@ const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric
 const formattedDate = now.toLocaleString('en-us', options);
 dateBox.textContent = "" + formattedDate;
 
-//
 
-const btn = document.getElementById('complete');
 
-btn.addEventListener('click', function () {
-  alert("Task Completed!!!");
+// 
+
+const completedBtn = document.querySelectorAll('.completed-btn');
+const taskCount = document.getElementById('task-count');
+const taskPending = document.getElementById('task-pending');
+
+completedBtn.forEach(btn => { btn.addEventListener('click', function () {
+  alert("WOW! Task Completed!!!");
+
+  let Count = parseInt(taskCount.textContent);
+  taskCount.textContent = Count + 1; 
+
+  let Pending = parseInt(taskPending.textContent);
+  taskPending.textContent = Pending - 1; 
   btn.disabled = true;
+});
+});
+
+// 
+const clearBtn = document.getElementById('clear-history-btn');
+const taskDiv = document.getElementById('task-div');
+
+clearBtn.addEventListener('click', function() {
+  taskDiv.innerHTML = "";
 })
+
+// 
