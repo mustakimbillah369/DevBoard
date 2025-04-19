d4ae712117b1a227e2986551f27b4c2a21b6ed01
mustakimbillah369 <billah.mustakim30401@gmail.com>
2025-04-19 18:37:15 +0600

    added navbar

---
+++ javascript/navbar.js
@@ -0,0 +1,3 @@
+document.getElementById('bg-btn').addEventListener('click', function () {
+  document.documentElement.style.backgroundColor = bgRandomColor();
+})
---
+++ javascript/utilities.js
@@ -0,0 +1,9 @@
+function bgRandomColor() {
+  const digits = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    let randomColor = Math.floor(Math.random() * 16);
+    color += digits[randomColor];
+  }
+  return color;
+}
