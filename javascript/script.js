f59934dc1383c8e9370ce6df5cedd050e11795dc
mustakimbillah369 <billah.mustakim30401@gmail.com>
2025-04-20 02:55:03 +0600

    added Activity Log section

--- javascript/script.js
+++ javascript/script.js
@@ -2,10 +2,9 @@ document.getElementById('bg-btn').addEventListener('click', function () {
   document.documentElement.style.backgroundColor = bgRandomColor();
 })
 
-document.getElementById('blog').addEventListener('click', function() {
+document.getElementById('blog').addEventListener('click', function () {
   window.location.href = "blog.html";
 })
-
 
 // 
 
@@ -16,32 +15,45 @@ const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric
 const formattedDate = now.toLocaleString('en-us', options);
 dateBox.textContent = "" + formattedDate;
 
-
-
 // 
 
+const btns = document.querySelectorAll('.completed-btn');
+const taskDiv = document.getElementById('task-div');
+
+btns.forEach(button => {
+  button.addEventListener('click', () => {
+
+    const taskTitle = button.closest('.card').querySelector('.task-title').innerText;
+
+    const newDiv = document.createElement('div');
+    newDiv.className = 'font-semibold bg-gray-300 p-4 rounded-lg mb-3';
+    newDiv.innerText = `You have completed the task "${taskTitle}" at 03:48:56 AM.`
+
+    taskDiv.appendChild(newDiv);
+});
+});
+
+// 
 const completedBtn = document.querySelectorAll('.completed-btn');
 const taskCount = document.getElementById('task-count');
 const taskPending = document.getElementById('task-pending');
 
-completedBtn.forEach(btn => { btn.addEventListener('click', function () {
-  alert("WOW! Task Completed!!!");
+completedBtn.forEach(btn => {
+  btn.addEventListener('click', function () {
+    alert("Board updated Successfully!");
 
-  let Count = parseInt(taskCount.textContent);
-  taskCount.textContent = Count + 1; 
+    let Count = parseInt(taskCount.innerText);
+    taskCount.innerText = Count + 1;
 
-  let Pending = parseInt(taskPending.textContent);
-  taskPending.textContent = Pending - 1; 
-  btn.disabled = true;
-});
+    let Pending = parseInt(taskPending.innerText);
+    taskPending.innerText = Pending - 1;
+    btn.disabled = true;
+  });
 });
 
 // 
 const clearBtn = document.getElementById('clear-history-btn');
-const taskDiv = document.getElementById('task-div');
-
-clearBtn.addEventListener('click', function() {
-  taskDiv.innerHTML = "";
+const taskdiv = document.getElementById('task-div');
+clearBtn.addEventListener('click', function ()  {
+  taskdiv.innerHTML = "";
 })
-
-// 
--- javascript/utilities.js
+++ javascript/utilities.js
@@ -7,3 +7,5 @@ function bgRandomColor() {
   }
   return color;
 }
+
+// 
